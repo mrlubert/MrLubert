73abb0cae18b3c7acd73059a1add3a458bbc8f5c
mrlubert <mrlubert@users.noreply.github.com>
2020-07-06 14:32:55 -0400

    .

--- src/com/MrLubert/mrlubert/Main.java
+++ src/com/MrLubert/mrlubert/Main.java
@@ -9,56 +9,48 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-
-
-
 public class Main extends JavaPlugin implements Listener {
 
 	public TabManager tab;
 
 	@EventHandler
-    public void onPlayerQuit(PlayerQuitEvent e) {
-		
-        }
-	
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent e) {
+	public void onPlayerQuit(PlayerQuitEvent e) {
 
-        }
+	}
 
 	@EventHandler
-    public void onPlayerKick(PlayerKickEvent e) {
+	public void onPlayerJoin(PlayerJoinEvent e) {
 
-        }
+	}
+
+	@EventHandler
+	public void onPlayerKick(PlayerKickEvent e) {
+
+	}
 
 	@Override
 	public void onEnable() {
 		getLogger().info("Plugin Enabled!");
-        PluginManager pm = getServer().getPluginManager();
-        pm.registerEvents(this, this);
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(this, this);
 
-		
-        this.tab = new TabManager(this);
-		
+		this.tab = new TabManager(this);
+
 		tab.addHeader("&b&lSketch's Sub Server");
 		tab.addHeader("&b&lSketch's Sub Server");
 		tab.addHeader("&b&lSketch's Sub Server");
 		tab.addHeader("&5&lSketch's Sub Server");
 		tab.addHeader("&5&lSketch's Sub Server");
 		tab.addHeader("&5&lSketch's Sub Server");
-		
+
 		tab.addFooter("&bPlayers Online: &b" + Bukkit.getOnlinePlayers().size() + " &8&l/ &b" + Bukkit.getMaxPlayers());
-		
+
 		tab.showTab();
 	}
-	
+
 	@Override
 	public void onDisable() {
 		getLogger().info("Plugin Disabled!");
 	}
-	
+
 }
-
-
-
-
--- src/com/MrLubert/mrlubert/TabManager.java
+++ src/com/MrLubert/mrlubert/TabManager.java
@@ -12,78 +12,74 @@ import net.minecraft.server.v1_16_R1.ChatComponentText;
 import net.minecraft.server.v1_16_R1.PacketPlayOutPlayerListHeaderFooter;
 
 public class TabManager {
-	
+
 	private List<ChatComponentText> headers = new ArrayList<>();
 	private List<ChatComponentText> footers = new ArrayList<>();
-	
+
 	private Main plugin;
+
 	public TabManager(Main plugin) {
 		this.plugin = plugin;
 	}
-	
+
 	public void showTab() {
 		if (headers.isEmpty() && footers.isEmpty())
 			return;
-		
+
 		Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
-			
+
 			PacketPlayOutPlayerListHeaderFooter packet = new PacketPlayOutPlayerListHeaderFooter();
 			int count1 = 0; // header
 			int count2 = 0; // footer
-			
+
 			@Override
 			public void run() {
 				try {
-					
+
 					Field a = packet.getClass().getDeclaredField("header");
 					a.setAccessible(true);
-					
+
 					Field b = packet.getClass().getDeclaredField("footer");
 					b.setAccessible(true);
-					
-					if(count1 >= headers.size())
+
+					if (count1 >= headers.size())
 						count1 = 0;
-					
-					if(count2 >= footers.size())
+
+					if (count2 >= footers.size())
 						count2 = 0;
-					
+
 					a.set(packet, headers.get(count1));
-					
+
 					b.set(packet, footers.get(count2));
-					
+
 					count1++;
 					count2++;
-					
+
 					System.out.println(ChatColor.DARK_RED + "Loop");
-					
-					
-					{
-						for (Player player : Bukkit.getOnlinePlayers())
-							((CraftPlayer)player).getHandle().playerConnection.sendPacket(packet);
-						System.out.println(ChatColor.DARK_RED + "Sent Packet");
-				}
-					
+
+					for (Player player : Bukkit.getOnlinePlayers())
+						((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+					System.out.println(ChatColor.DARK_RED + "Sent Packet");
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-				
+
 			}
-			
+
 		}, 10, 40);
 	}
 
 	public void addHeader(String header) {
 		headers.add(new ChatComponentText(format(header)));
 	}
-	
+
 	public void addFooter(String footer) {
 		footers.add(new ChatComponentText(format(footer)));
 	}
-	
+
 	private String format(String msg) {
 		return ChatColor.translateAlternateColorCodes('&', msg);
 	}
-	
-	
 
 }
