a88528f3f167dcd9c5fb68113487196c5f16c7c8
mrlubert <mrlubert@users.noreply.github.com>
2020-07-06 14:42:01 -0400

    .

--- src/com/MrLubert/mrlubert/TabManager.java
+++ src/com/MrLubert/mrlubert/TabManager.java
@@ -50,7 +50,7 @@ public class TabManager {
 
 					a.set(packet, headers.get(count1));
 
-					b.set(packet, footers.get(count2));
+					b.set(packet, "&bPlayers Online: &b" + Bukkit.getOnlinePlayers().size() + " &8&l/ &b" + Bukkit.getMaxPlayers());
 
 					count1++;
 					count2++;
