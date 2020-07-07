21c21471b761165c3f51561bd6601301e6a039c2
mrlubert <mrlubert@users.noreply.github.com>
2020-07-07 07:26:31 -0400

    7/7/20-7:26 am EST*

--- src/com/MrLubert/mrlubert/Update.java
+++ src/com/MrLubert/mrlubert/Update.java
@@ -7,10 +7,4 @@ public class Update {
 	public final int PlayerCount = Bukkit.getOnlinePlayers().size();
 	public int playersOnline = Bukkit.getOnlinePlayers().size();
 	
-	public int PlayerCount() {
-		if (Bukkit.getOnlinePlayers().size() != 0) {
-			Bukkit.broadcastMessage("PLAYERS ONLINE: " + Bukkit.getOnlinePlayers().size());
-	}
-		return Bukkit.getOnlinePlayers().size();
-  }
 }
